98545db684c8db7b3769db5cead45b7b93d2fa99
stephensir <stephensir@gmail.com>
2019-01-25 20:37:47 +0800

    update

--- Todo/app/src/main/java/com/example/todo/MainActivity.java
+++ Todo/app/src/main/java/com/example/todo/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private String TAG = "Todo===>";
     private String[] mTodos;
     private int mTodoIndex = 0;
+    private static final String TODO_INDEX = "todoIndex";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +25,13 @@ public class MainActivity extends AppCompatActivity {
         // set the user interface layout for this Activity
         // the layout file is defined in the project res/layout/activity_todo.xml file
         setContentView(R.layout.activity_main);
+        // check for saved state due to changes such as rotation or back button
+        // and restore any saved state such as the todo index
+        if (savedInstanceState != null){
+            mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
+        }
 
-        Log.d(TAG,"onCreate()");
+        Log.d(TAG,"onCreate");
 
         // initialize member TextView so we can manipulate it later
         final TextView TodoTextView;
@@ -41,45 +47,51 @@ public class MainActivity extends AppCompatActivity {
         buttonNext = (Button) findViewById(R.id.buttonNext);
 
         // OnClick listener for the  Next button
-        buttonNext.setOnClickListener(new View.OnClickListener() {
+        buttonNext.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                //mTodoIndex += 1;
+                // mTodoIndexx += 1;
                 mTodoIndex = (mTodoIndex + 1) % mTodos.length;
                 TodoTextView.setText(mTodos[mTodoIndex]);
             }
         });
-    } //onCreate
+    } //onCreate()
 
+    // Serialize State
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
+    } //onSaveInstanceState()
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG,"onStart()");
-    } //onStart
+        Log.d(TAG,"onStart");
+    } //onStart()
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG,"onResume()");
-    } //onResume
+        Log.d(TAG,"onResume");
+    } //onResume()
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG,"onPause()");
-    } //onPause
+        Log.d(TAG,"onPause");
+    } //onPause()
 
     @Override
     protected void onStop() {
-        super.onStop();
-        Log.d(TAG,"onStop()");
-    } //onStop
+        super.onStop ();
+        Log.d(TAG,"onStop ");
+    } //onStop ()
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG,"onDestroy()");
-    } //onDestroy
+        super.onDestroy ();
+        Log.d(TAG,"onDestroy ");
+    } //onDestroy()
 
 }
